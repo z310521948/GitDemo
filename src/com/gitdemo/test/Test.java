db660c531d94c35019e96c973bdb659d12a0eaa7
z310521948 <z310521948@qq.com>
2017-02-14 12:08:31 +0800

    冲突测试

--- src/com/gitdemo/test/Test.java
+++ src/com/gitdemo/test/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test");
-		System.out.println("hello");
+		System.out.println("hello1");
 	}
 
 }
