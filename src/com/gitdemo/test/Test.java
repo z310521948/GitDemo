09e88656b3bc474f155777d4a0305aa4dcd4336a
z310521948 <z310521948@qq.com>
2017-02-15 17:26:15 +0800

    Update Test.java

--- src/com/gitdemo/test/Test.java
+++ src/com/gitdemo/test/Test.java
@@ -4,12 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test");
-		System.out.println("hello1");
-		System.out.println("hello2");
-		System.out.println("hello3");
-		System.out.println("hello4");
-		System.out.println("hello5");
+		System.out.println("test1");
 	}
 
 }
